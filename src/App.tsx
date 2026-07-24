4fb48f0092d4ad0f66cdd0010f9f8bd9e0e8d646
abdallahsultan003 <abdallahsultan00@gmail.com>
2026-07-25 00:49:51 +0300

    implement cloudinary for image upload

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,11 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
 
 function App() {
   return (
@@ -42,6 +44,12 @@ function App() {
                       list: '/subjects',
                       create: '/subjects/create',
                       meta: { label: 'Subjects', icon: <BookOpen />}
+                  }, {
+                      name: 'classes',
+                      list: '/classes',
+                      create: '/classes/create',
+                      show: '/classes/show/:id',
+                      meta: { label: 'Classes', icon: <GraduationCap />}
                   }
               ]}
             >
@@ -57,6 +65,11 @@ function App() {
                       <Route path="subjects">
                           <Route index element={<SubjectsList />} />
                           <Route path="create" element={<SubjectsCreate />} />
+                      </Route>
+
+                      <Route path="classes">
+                          <Route index element={<ClassesList />} />
+                          <Route path="create" element={<ClassesCreate />} />
                       </Route>
                 </Route>
               </Routes>
